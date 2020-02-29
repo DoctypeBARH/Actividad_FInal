0e4d0cbfecaa062699e6e4632f01f0c234f2ed7a
BARH <brau_chulo@hotmail.com>
2020-02-28 19:57:41 -0600

    creacion de mi primer funcion inicia para recibir varios atributos

--- MOVIES.py
+++ MOVIES.py
@@ -1,5 +1,12 @@
 class Movie:
     """Esta es la clases que se encargara de darle los rangos 
-        , los likes y los dislikes desde varios metodos"""
-
+        , los likes y los dislikes desde varios metodos
+         - Crear una clase llamada Movie (documentar la clase)
+         - La clase tiene los siguientes atributos:
+         - Title - Titulo de la pelicula en minusculas
+         - Rank - Rango inicial de la pelicula un random de 0 a 10
+    """
+    def __init__ (self,titulo,rango):
+        self.titulo=titulo
+        self.rango=rango
     pass
